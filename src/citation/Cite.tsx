b2c21ccf1e211721a19d991b2a8b426883fb9e44
Brian Takita <brian.takita@gmail.com>
2023-10-22 05:55:53 -0400

    - <Main_about>
    <Cite>: $p:
    
    	uuid→id
    	+ innerHTML
    
    update dependencies

--- src/citation/Cite.tsx
+++ src/citation/Cite.tsx
@@ -1,15 +1,20 @@
 import { cite } from '@btakita/domain--server--blog'
 import { type Ctx } from '@ctx-core/object'
 import { ctx__Context__use } from '@ctx-core/solid-js'
-import { type ParentProps } from 'solid-js'
+import { createMemo, type ParentProps } from 'solid-js'
 import { renderToString } from 'solid-js/web'
 import { _citation__handle__html_id__new, _citation__html_id__new } from './_citation__html_id.ts'
 export function Cite($p:ParentProps<{
 	ctx?:Ctx
-	uuid:string // handle Astrojs progressive rendering
+	id:string, // handle Astrojs progressive rendering
+	innerHTML?:string
 }>) {
 	const ctx = $p.ctx || ctx__Context__use()
-	const citation = cite(ctx, $p.uuid, renderToString(()=>$p.children))
+	const citation =
+		cite(
+			ctx,
+			$p.id,
+			$p.innerHTML || renderToString(()=>$p.children))
 	return (
 		<sup>
 			<a
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from './about'
 export * from './anchor'
 export * from './breadcrumb'
 export * from './chidren'
