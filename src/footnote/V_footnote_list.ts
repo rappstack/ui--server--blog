1da4fd7dcc9b76ecc74bd672bc336a00029105a9
Brian Takita <brian.takita@gmail.com>
2023-11-08 20:02:19 -0500

    V_footnote_list:
    	H.div instead of V_fragment: + set class on parent element
    	props: + class?:string
    	V→van_T

--- src/footnote/V_footnote_list.ts
+++ src/footnote/V_footnote_list.ts
@@ -3,16 +3,20 @@ import { H_, V_fragment, V_raw } from '@ctx-core/vanjs'
 import { type Ctx } from '@ctx-core/object'
 import type { ChildDom, PlateVan } from 'van-type-delegate'
 import { _footnote__html_id__new } from './_footnote__html_id'
-export function V_footnote_list<V extends PlateVan>({ ctx }:{ ctx:Ctx }):ChildDom<V> {
+export function V_footnote_list<van_T extends PlateVan>({ ctx, ...$p }:{
+	ctx:Ctx
+	class?:string
+}):ChildDom<van_T> {
 	const citation_o = footnote_o_(ctx)
 	if (!citation_o) return
-	const H = H_<V>(ctx)
+	const H = H_<van_T>(ctx)
 	return (
-		V_fragment<V, ChildDom<V>>(ctx,
+		H.div({
+			class: $p.class
+		},
 			...footnote_o_(ctx).footnote_a.map(footnote=>
 				H.p({ id: _footnote__html_id__new(footnote) },
 					`[${footnote.seq}]: `,
-					H.em(V_raw<V, ChildDom<V>>(ctx, footnote.html))))
-		)
+					H.em(V_raw<van_T, ChildDom<van_T>>(ctx, footnote.html)))))
 	)
 }
