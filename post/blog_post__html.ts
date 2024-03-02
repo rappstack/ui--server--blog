3ce6867870d6a5f124386bf6bf88cfaf6b07aea8
Brian Takita <brian.takita@gmail.com>
2024-03-02 14:06:10 -0500

    ./post:
    	blog_post__text_: ∋ response__drain
    	+ blog_post__estimate_read_minutes__wait
    
    ./social: socials__div_: + class?:string

--- post/blog_post__html.ts
+++ post/blog_post__html.ts
@@ -1,6 +1,7 @@
 import { blog_post__tag_ } from '@rappstack/domain--server--blog/post'
-import { nullish__none_, run } from 'ctx-core/function'
-import { type request_ctx_T } from 'relysjs/server'
+import { response__drain } from '@rappstack/domain--server/response'
+import { nullish__none_ } from 'ctx-core/function'
+import { type request_ctx_T, rmemo__wait } from 'relysjs/server'
 import { id_be_memo_pair_ } from 'rmemo'
 export const [
 	,
@@ -30,13 +31,9 @@ export const [
 							blog_post__text += text.text
 						}
 					})
-				const response = rw.transform(new Response(blog_post__html))
-				run(async ()=>{
-					const reader = response.body!.getReader()
-					while (!(await reader.read()).done) {
-						/* empty */
-					}
-				}).then(()=>{
+				response__drain(
+					rw.transform(new Response(blog_post__html))
+				).then(()=>{
 					blog_post__text$._ = blog_post__text
 				}).catch(err=>console.error(err))
 				return blog_post__text$.val
@@ -49,5 +46,7 @@ export const [
 	(ctx:request_ctx_T)=>
 		nullish__none_([blog_post__text_(ctx)],
 			blog_post__text=>
-				Math.ceil(blog_post__text!.split(/\s+/).length / 200))
-)
+				Math.ceil(blog_post__text!.split(/\s+/).length / 200)))
+export function blog_post__estimate_read_minutes__wait(ctx:request_ctx_T) {
+	return rmemo__wait(()=>blog_post__estimate_read_minutes_(ctx), $=>$ != null, 5_000)
+}
--- social/socials__div.ts
+++ social/socials__div.ts
@@ -2,25 +2,31 @@ import { social_a1_ } from '@rappstack/domain--server/social'
 import { button__a_ } from '@rappstack/ui--any/anchor'
 import { class_ } from 'ctx-core/html'
 import { type relement_env_T, type wide_ctx_T } from 'relementjs'
+import type { tag_props_T } from 'relementjs/any'
 import { div_ } from 'relementjs/html'
 export function socials__div_<env_T extends relement_env_T>({
 	ctx,
 	centered,
 	link_button_class,
+	class:_class,
+	...div_props
 }:{
 	ctx:wide_ctx_T
 	centered?:boolean
 	link_button_class?:string
-}) {
+	class?:string
+}&tag_props_T<HTMLDivElement>) {
 	return (
 		div_<env_T>({
+			...div_props,
 			class: class_(
 				'Socials',
 				'social-icons',
 				centered ? 'flex' : null,
 				'flex-wrap',
 				'justify-center',
-				'gap-1')
+				'gap-1',
+				_class)
 		}, ...social_a1_(ctx).map(social=>
 			button__a_({
 				href: social.href,
