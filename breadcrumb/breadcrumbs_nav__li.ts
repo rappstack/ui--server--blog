f31ef4adef838a4b5f5b24276e196a626fd2a79f
Brian Takita <brian.takita@gmail.com>
2024-03-09 18:25:32 -0500

    ./breadcrumb: breadcrumbs_nav_:
    	∋ breadcrumb_a1_
    	∋ BreadcrumbList_id_ref_

--- breadcrumb/breadcrumbs_nav__li.ts
+++ breadcrumb/breadcrumbs_nav__li.ts
@@ -1,58 +1,20 @@
-import { jsonld__add } from '@rappstack/domain--server/jsonld'
-import { site__title_ } from '@rappstack/domain--server/site'
-import { request_url__origin_ } from '@rappstack/ui--server/request'
+import { breadcrumb_a1_, BreadcrumbList_id_ref_ } from '@rappstack/domain--server--blog/breadcrumb'
 import { class_ } from 'ctx-core/html'
-import { isNumber_ } from 'ctx-core/number'
-import { url__join } from 'ctx-core/uri'
-import { raw_, type relement_env_T } from 'relementjs'
-import { a_, li_, nav_, script_, span_, ul_ } from 'relementjs/html'
-import { type request_ctx_T, request_url_ } from 'relysjs/server'
-import type { BreadcrumbList } from '@btakita/schema-dts'
+import { type relement_env_T } from 'relementjs'
+import { a_, li_, nav_, span_, ul_ } from 'relementjs/html'
+import { type request_ctx_T } from 'relysjs/server'
 export function breadcrumbs__nav_<env_T extends relement_env_T>({
 	ctx
 }:{
 	ctx:request_ctx_T
 }) {
-	// Remove current url path and remove trailing slash if exists
-	const current_url_path:string = request_url_(ctx)
-		.pathname
-		.replace(/\/+$/, '')
-	// Get url array from path
-	// eg: /tags/tailwindcss => ['tags', 'tailwindcss']
-	const breadcrumb_a = current_url_path.split('/').slice(1)
-	// if breadcrumb is Home > Posts > 1 <etc>
-	// replace Posts with Posts (page number)
-	if (breadcrumb_a[0] === 'posts' && isNumber_(breadcrumb_a[1]) && !breadcrumb_a[1].includes('-')) {
-		breadcrumb_a.splice(0, 2, `Posts (page ${breadcrumb_a[1] || 1})`)
-	}
-	if (!breadcrumb_a.length) return
+	if (!breadcrumb_a1_(ctx).length) return
 	const li_class = class_(
 		'inline',
 		'capitalize',
 		'opacity-80',
 		'[&:not(:last-child)]:hover:opacity-100')
-	jsonld__add(ctx, <BreadcrumbList>{
-		'@context': 'https://schema.org',
-		'@type': 'BreadcrumbList',
-		'@id': url__join(request_url__origin_(ctx), '#BreadcrumbList'),
-		name: 'BreadcrumbList | ' + site__title_(ctx),
-		itemListElement: [
-			{
-				'@type': 'ListItem',
-				'@id': url__join(request_url__origin_(ctx), '#BreadcrumbList_home'),
-				position: 1,
-				name: 'Home',
-				item: request_url__origin_(ctx)
-			},
-			...breadcrumb_a.map((breadcrumb, idx)=>({
-				'@type': 'ListItem',
-				'@id': url__join(request_url__origin_(ctx), `#BreadcrumbList_${breadcrumb.replaceAll('/', '_')}`),
-				position: idx + 2,
-				name: breadcrumb,
-				item: url__join(request_url__origin_(ctx), breadcrumb)
-			}))
-		]
-	})
+	BreadcrumbList_id_ref_(ctx)
 	return [
 		nav_<env_T>({
 			id: 'breadcrumb',
@@ -78,11 +40,11 @@ export function breadcrumbs__nav_<env_T extends relement_env_T>({
 					}, 'Home'),
 					span_({ 'aria-hidden': true }, ' > ')
 				]),
-				...breadcrumb_a.map((breadcrumb, idx)=>
+				...breadcrumb_a1_(ctx).map((breadcrumb, idx)=>
 					li_({
 						class: li_class,
 					}, [
-						idx + 1 === breadcrumb_a.length
+						idx + 1 === breadcrumb_a1_(ctx).length
 							? span_({ class: idx > 0 ? 'lowercase' : 'capitalize', 'aria-current': 'page' },
 								breadcrumb)
 							: [
