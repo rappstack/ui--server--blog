8e0d8eefd6e47a82c03d5cca31dbcfede3636943
Brian Takita <brian.takita@gmail.com>
2024-03-25 16:56:19 -0400

    ./main: blog__main_fragment_:
    	+ hero_class?:string
    	+ hero div: contains: h1,description p

--- main/blog__main_fragment.ts
+++ main/blog__main_fragment.ts
@@ -1,11 +1,12 @@
 import { class_, dataset__data_attrs_ } from 'ctx-core/html'
 import { fragment_, type relement_env_T, type tag_dom_T } from 'relementjs'
-import { h1_, h2_, main_, p_ } from 'relementjs/html'
+import { div_, h1_, h2_, main_, p_ } from 'relementjs/html'
 import { type request_ctx_T } from 'relysjs/server'
 import { breadcrumbs__nav_ } from '../breadcrumb/index.js'
 export function blog__main_fragment_<env_T extends relement_env_T>({
 	ctx,
 	class: _class,
+	hero_class,
 	h1_text,
 	h1_class,
 	description_class,
@@ -15,6 +16,7 @@ export function blog__main_fragment_<env_T extends relement_env_T>({
 }:{
 	ctx:request_ctx_T
 	class?:string
+	hero_class?:string
 	h1_text?:string
 	h1_class?:string
 	description_class?:string
@@ -36,14 +38,20 @@ export function blog__main_fragment_<env_T extends relement_env_T>({
 			hyop,
 			...dataset__data_attrs_(dataset || {})
 		}, [
-			h1_text ? h1_({ class: h1_class }, h1_text) : null,
-			description
-				? p_({
-					class: class_(
-						'italic',
-						description_class)
-				}, description)
-				: null,
+			div_({
+				class: class_(
+					'hero',
+					hero_class ?? 'prose mb-8')
+			}, [
+				h1_text ? h1_({ class: h1_class }, h1_text) : null,
+				description
+					? p_({
+						class: class_(
+							'italic',
+							description_class)
+					}, description)
+					: null,
+			]),
 			...children
 		]))
 }
