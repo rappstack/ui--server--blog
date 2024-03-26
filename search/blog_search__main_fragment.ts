1c1d9aea774f914e47dd212d28a3199e5f61aa01
Brian Takita <brian.takita@gmail.com>
2024-03-25 22:08:57 -0400

    ./search: blog_search__main_fragment:
    	+ h1_text?:string
    	+ h1_class?:string

--- search/blog_search__main_fragment.ts
+++ search/blog_search__main_fragment.ts
@@ -4,13 +4,19 @@ import { type request_ctx_T } from 'relysjs/server'
 import { blog__main_fragment_ } from '../main/index.js'
 export function blog_search__main_fragment_<env_T extends relement_env_T>({
 	ctx,
+	h1_text,
+	h1_class,
 }:{
 	ctx:request_ctx_T
+	h1_text?:string
+	h1_class?:string
 }, ...children:tag_dom_T[]) {
 	return (
 		blog__main_fragment_<env_T>({
 			ctx,
 			class: 'blog_search__main_fragment',
+			h1_text,
+			h1_class,
 			/** @see {import('@rappstack/ui--browser--blog/search').blog_search__main__hyop} */
 			hyop: 'blog_search__main__hyop',
 			dataset: {
