2fa6b20929ba0333acb2da207bb9e373f8449d58
Brian Takita <brian.takita@gmail.com>
2024-04-11 17:53:55 -0400

    ./footnote: footnote_list__div_: return to handle in front of the [n]

--- footnote/footnote_list__div.ts
+++ footnote/footnote_list__div.ts
@@ -1,7 +1,7 @@
 import { footnote_o_ } from '@rappstack/domain--server--blog/footnote'
 import { class_ } from 'ctx-core/html'
 import { raw_, type relement_env_T, type wide_ctx_T } from 'relementjs'
-import { a_, div_, em_, p_, sub_ } from 'relementjs/html'
+import { a_, div_, em_, p_ } from 'relementjs/html'
 import { heroicons_uturn_up_ } from '../icon/index.js'
 import { _footnote__html_id__new, _footnote__ref__html_id__new } from './_footnote__html_id.js'
 export function footnote_list__div_<env_T extends relement_env_T>({
@@ -17,28 +17,21 @@ export function footnote_list__div_<env_T extends relement_env_T>({
 		div_<env_T>({ class: _class }, [
 			...footnote_o.footnote_a.map(footnote=>[
 				p_({ id: _footnote__html_id__new(footnote) }, [
-					'[' + footnote.seq + ']:',
-					em_(raw_(footnote.html)),
-					' ',
-					sub_({
+					a_({
+						href: '#' + _footnote__ref__html_id__new(footnote),
 						class: class_(
 							'inline-block',
-							'float-right')
-					},[
-						a_({
-							href: '#' + _footnote__ref__html_id__new(footnote),
+							'relative',
+							'ml-auto')
+					}, [
+						heroicons_uturn_up_({
 							class: class_(
-								'inline-block',
-								'relative',
-								'ml-auto')
-						}, [
-							heroicons_uturn_up_({
-								class: class_(
-									'h-3',
-									'w-3')
-							})
-						])
+								'h-3',
+								'w-3')
+						})
 					]),
+					'[' + footnote.seq + ']:',
+					em_(raw_(footnote.html)),
 				]),
 			]),
 		])
