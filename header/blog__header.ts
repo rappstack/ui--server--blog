8349653cb2a7fea41bbe9381fdc3a0d136d4e2ca
Brian Takita <brian.takita@gmail.com>
2024-05-31 19:11:26 -0400

    ./header: blog__header_:
    	+ a_class?:string
    	+ nav_container_class?:string

--- header/blog__header.ts
+++ header/blog__header.ts
@@ -6,18 +6,24 @@ import { type tag_props_T } from 'relementjs/any'
 import { a_, button_, div_, header_, nav_, ul_ } from 'relementjs/html'
 import { line_, svg_ } from 'relementjs/svg'
 import { hr_div_ } from '../hr/index.js'
-export function blog__header_<env_T extends relement_env_T>({
-	ctx,
-	class: _class,
-	is_open,
-	logo_image_class,
-	...header_props
-}:{
+type blog__header_props_T = {
 	ctx:wide_ctx_T
 	class?:string
+	a_class?:string
+	nav_container_class?:string
 	is_open?:boolean
 	logo_image_class?:string
-}&tag_props_T<HTMLElement>, ...children:tag_dom_T[]) {
+}&tag_props_T<HTMLElement>
+export function blog__header_<env_T extends relement_env_T>($p:blog__header_props_T, ...children:tag_dom_T[]) {
+	let {
+		ctx,
+		class: _class,
+		a_class,
+		nav_container_class,
+		is_open,
+		logo_image_class,
+		...header_props
+	} = $p
 	logo_image_class ??= 'w-16 h-10'
 	return (
 		header_<env_T>({
@@ -28,7 +34,7 @@ export function blog__header_<env_T extends relement_env_T>({
 		}, [
 			a_({
 				id: 'skip-to-content',
-				class: class_(
+				class: a_class ?? class_(
 					'absolute',
 					'z-50',
 					'-top-full',
@@ -44,17 +50,11 @@ export function blog__header_<env_T extends relement_env_T>({
 			div_({
 				class: class_(
 					'nav-container',
-					'mx-auto',
-					'flex',
-					'max-w-3xl',
-					'flex-col',
-					'sm:flex-row',
-					'items-center',
-					'justify-between')
-			}, [
-				div_({
-					class: class_(
+					nav_container_class ?? [
 						'nofouc',
+						'mx-auto',
+						'flex',
+						'max-w-3xl',
 						'top-nav-wrap',
 						'relative',
 						'flex',
@@ -63,86 +63,85 @@ export function blog__header_<env_T extends relement_env_T>({
 						'justify-between',
 						'w-full',
 						'p-4',
-						'sm:py-8'),
+					],)
+			}, [
+				a_({
+					href: '/',
+					title: 'Home | ' + site__title_(ctx),
+					class: class_(
+						'logo',
+						'absolute',
+						'sm:static',
+						'font-semibold',
+						'text-xl',
+						'sm:text-2xl',
+						'whitespace-nowrap'),
 				}, [
-					a_({
-						href: '/',
-						title: 'Home | ' + site__title_(ctx),
+					logo_image_(ctx, {
 						class: class_(
-							'logo',
-							'absolute',
-							'sm:static',
-							'font-semibold',
-							'text-xl',
-							'sm:text-2xl',
-							'whitespace-nowrap'),
+							'fill-current',
+							'stroke-current',
+							'text-current',
+							logo_image_class)
+					}) ?? site__title_(ctx)
+				]),
+				nav_({
+					id: 'blog__header__handle__nav',
+					class: class_(
+						'group/blog__header__handle__nav',
+						is_open ? 'is-open' : null,
+						'flex',
+						'w-full',
+						'flex-col',
+						'sm:flex-row',
+						'items-center',
+						'sm:justify-end',
+						'sm:ml-2',
+						'sm:py-0',
+						'sm:space-x-4'),
+					/** @see {import('@rappstack/ui--browser--blog/header').blog__header__handle__nav__hyop} */
+					hyop: 'blog__header__handle__nav__hyop'
+				}, [
+					button_({
+						class: class_(
+							'menu_handle',
+							'sm:hidden',
+							'self-end',
+							'p-2',
+							'focus-outline'),
+						'aria-label': 'Open Menu',
+						'aria-expanded': false,
+						'aria-controls': 'menu-items',
+						/** @see {import('@rappstack/ui--browser--blog/header').blog__header__handle__nav__button__hyop} */
+						hyop: 'blog__header__handle__nav__button__hyop'
 					}, [
-						logo_image_(ctx, {
+						menu_icon__svg_({
 							class: class_(
-								'fill-current',
-								'stroke-current',
-								'text-current',
-								logo_image_class)
-						}) ?? site__title_(ctx)
+								'inline-block',
+								'h-6',
+								'w-6',
+								'fill-skin-base',
+								'group-hover:fill-skin-accent')
+						})
 					]),
-					nav_({
-						id: 'blog__header__handle__nav',
+					ul_({
+						id: 'menu-items',
 						class: class_(
-							'group/blog__header__handle__nav',
-							is_open ? 'is-open' : null,
-							'flex',
-							'w-full',
+							is_open ? 'flex' : 'hidden',
+							'group-[.is-open]/blog__header__handle__nav:flex',
+							'sm:flex',
 							'flex-col',
 							'sm:flex-row',
-							'items-center',
-							'sm:justify-end',
-							'sm:ml-2',
-							'sm:py-0',
-							'sm:space-x-4'),
-						/** @see {import('@rappstack/ui--browser--blog/header').blog__header__handle__nav__hyop} */
-						hyop: 'blog__header__handle__nav__hyop'
-					}, [
-						button_({
-							class: class_(
-								'menu_handle',
-								'sm:hidden',
-								'self-end',
-								'p-2',
-								'focus-outline'),
-							'aria-label': 'Open Menu',
-							'aria-expanded': false,
-							'aria-controls': 'menu-items',
-							/** @see {import('@rappstack/ui--browser--blog/header').blog__header__handle__nav__button__hyop} */
-							hyop: 'blog__header__handle__nav__button__hyop'
-						}, [
-							menu_icon__svg_({
-								class: class_(
-									'inline-block',
-									'h-6',
-									'w-6',
-									'fill-skin-base',
-									'group-hover:fill-skin-accent')
-							})
-						]),
-						ul_({
-							id: 'menu-items',
-							class: class_(
-								is_open ? 'flex' : 'hidden',
-								'group-[.is-open]/blog__header__handle__nav:flex',
-								'sm:flex',
-								'flex-col',
-								'sm:flex-row',
-								'w-screen',
-								'sm:w-auto',
-								'mt-4',
-								'sm:mt-0',
-								'sm:ml-0',
-								'gap-x-2',
-								'sm:gap-x-5',
-								'gap-y-2',
-								'sm:gap-y-0')
-						}, ...children)
-					])
+							'w-screen',
+							'sm:w-auto',
+							'mt-4',
+							'sm:mt-0',
+							'sm:ml-0',
+							'gap-x-2',
+							'sm:gap-x-5',
+							'gap-y-2',
+							'sm:gap-y-0')
+					}, ...children)
 				])
 			]),
 			hr_div_({
